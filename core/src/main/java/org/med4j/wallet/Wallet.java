f491b198a03000320c66bbe92aaeff96ff516549
Yohan <yohanpark@medibloc.org>
2018-11-27 23:01:13 +0900

    [Wallet] implementing create() method
    - use generateKeysFromPassphrase() to generate new key pair

--- core/src/main/java/org/med4j/wallet/Wallet.java
+++ core/src/main/java/org/med4j/wallet/Wallet.java
@@ -1,7 +1,10 @@
 package org.med4j.wallet;
 
+import org.med4j.crypto.ECKeyPair;
+
 public class Wallet {
     private WalletFile walletFile;
+    private ECKeyPair ecKeyPair;
 
     public WalletFile getWalletFile() {
         return walletFile;
@@ -9,6 +12,14 @@ public class Wallet {
 
     public void setWalletFile(WalletFile walletFile) {
         this.walletFile = walletFile;
+    }
+
+    public ECKeyPair getEcKeyPair() {
+        return ecKeyPair;
+    }
+
+    public void setEcKeyPair(ECKeyPair ecKeyPair) {
+        this.ecKeyPair = ecKeyPair;
     }
 
     public void validatePassword(String password) {
--- core/src/main/java/org/med4j/wallet/WalletCreator.java
+++ core/src/main/java/org/med4j/wallet/WalletCreator.java
@@ -1,32 +1,28 @@
 package org.med4j.wallet;
 
 import org.med4j.crypto.ECKeyPair;
+import org.med4j.crypto.Keys;
 
 public class WalletCreator {
-    /** Create new key pair and make a wallet file. */
-    public static Wallet create(String password) {
-
-        create(password, null, null);
-        return null;
-    }
-
-    /** Create new key pair and make a wallet file. */
-    public static Wallet create(String password, String keyDirectory) {
-        create(password, keyDirectory, null);
-        return null;
-    }
-
-    /** Create new wallet file for the existing key pair. */
-    public static Wallet create(String password, ECKeyPair ecKeyPair) {
-        create(password, null, ecKeyPair);
-        return null;
-    }
-
-    public static Wallet create(String password, String keyDirectory, ECKeyPair ecKeyPair) {
+    /**
+     * Create new wallet.
+     *
+     * @param password required
+     * @param keyDirectory optional(if null, use default key directory)
+     * @param existingKeyPair optional(if null, generate new key pair)
+     * @return New Wallet
+     */
+    public static Wallet create(String password, String keyDirectory, ECKeyPair existingKeyPair) throws Exception {
         Wallet wallet = new Wallet();
         wallet.validatePassword(password);
         wallet.setWalletFile(new WalletFile(keyDirectory));
 
-        return null;
+        ECKeyPair ecKeyPair = existingKeyPair;
+        if (ecKeyPair == null) {
+            ecKeyPair = Keys.generateKeysFromPassphrase(password);
+        }
+        wallet.setEcKeyPair(ecKeyPair);
+
+        return wallet;
     }
 }
