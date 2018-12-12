2f46ed38084808b6f21a2da5e0569ea7e017bf98
Yohan <yohanpark@medibloc.org>
2018-12-12 20:58:55 +0900

    [Transaction] add getSendTransactionRequest()

--- core/src/main/java/org/med4j/tx/Transaction.java
+++ core/src/main/java/org/med4j/tx/Transaction.java
@@ -1,14 +1,52 @@
 package org.med4j.tx;
 
-import org.med4j.core.protobuf.BlockChain;
+import com.google.protobuf.ByteString;
+import org.med4j.account.Account;
+import org.med4j.core.protobuf.BlockChain.TransactionHashTarget;
+import org.med4j.core.protobuf.Rpc;
 import org.med4j.crypto.Hash;
 import org.med4j.utils.Numeric;
 
 public class Transaction {
     static final int VALUE_SIZE = 16;
 
-    public static String hashTx(BlockChain.TransactionHashTarget hashTarget) {
+    public static String hashTx(TransactionHashTarget hashTarget) {
         byte[] bytes = hashTarget.toByteArray();
         return Numeric.byteArrayToHex(Hash.sha3256(bytes));
+    }
+
+    public static Rpc.SendTransactionRequest getSendTransactionRequest(Account account, byte[] data) {
+        TransactionHashTarget txHashTarget = TransactionHashTarget.newBuilder()
+                .setNonce(1)
+                .setPayload(ByteString.copyFrom(data))
+                .setFrom(ByteString.copyFromUtf8(account.getAddress()))
+                .setChainId(181112)
+                .setTimestamp(1544610216)
+                //.setTo(null)
+                .setValue(ByteString.copyFromUtf8("0"))
+                .setTxType("add_record")
+                .build();
+
+        // TODO - should be "e48c476c2fc1a21eee2a25a070bdb1f69788d6c0c3c84f7cfb84abae6868d9e3"
+        String hash = hashTx(txHashTarget);
+        // TODO - should be "2bca8e8dc3faef143018284f3e9aba02af375c9f0693c44fab8ff14dd41c3aee029167bb360413175cc40fe80a40a35e9b10f96aedcea0b6a85289a9dc53083300"
+        String sign = "";
+
+        Rpc.SendTransactionRequest request = Rpc.SendTransactionRequest.newBuilder()
+                .setHashAlg(2)
+                .setHash(hash)
+                .setChainId(181112)
+                .setCryptoAlg(1)
+                .setNonce(1)
+                .setTimestamp(1544610216)
+                //.setPayerSign(null)
+                .setPayload("abc")
+                .setSign(sign)
+                .setTo(account.getAddress())
+                .setTxType("add_record")
+                .setValue("0")
+                .build();
+
+        return request;
     }
 }
--- core/src/test/java/org/med4j/core/TransactionTest.java
+++ core/src/test/java/org/med4j/core/TransactionTest.java
@@ -4,10 +4,14 @@ import com.google.protobuf.ByteString;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.med4j.account.Account;
+import org.med4j.account.AccountUtils;
 import org.med4j.core.protobuf.BlockChain;
+import org.med4j.core.protobuf.Rpc;
 import org.med4j.tx.Transaction;
 import org.med4j.utils.Numeric;
 
+import java.io.File;
 import java.math.BigInteger;
 
 public class TransactionTest {
@@ -55,5 +59,31 @@ public class TransactionTest {
         builder.setTxType("transfer");
         String hash = Transaction.hashTx(builder.build());
         System.out.println(hash);
+    }
+
+    @Test
+    public void testGetSendTransactionRequest() throws Exception {
+        Account account = AccountUtils.loadAccount(new File("sampleAccount.json"));
+
+        Rpc.SendTransactionRequest expected
+                = Rpc.SendTransactionRequest.newBuilder()
+                .setHashAlg(2)
+                .setHash("e48c476c2fc1a21eee2a25a070bdb1f69788d6c0c3c84f7cfb84abae6868d9e3")
+                .setChainId(181112)
+                .setCryptoAlg(1)
+                .setNonce(1)
+                .setTimestamp(1544610216)
+                //.setPayerSign(null)
+                .setPayload("abc")
+                .setSign("2bca8e8dc3faef143018284f3e9aba02af375c9f0693c44fab8ff14dd41c3aee029167bb360413175cc40fe80a40a35e9b10f96aedcea0b6a85289a9dc53083300")
+                .setTo(account.getAddress())
+                .setTxType("add_record")
+                .setValue("0")
+                .build();
+
+
+        Rpc.SendTransactionRequest actual = Transaction.getSendTransactionRequest(account, "abc".getBytes());
+
+        assertEquals(expected, actual);
     }
 }
