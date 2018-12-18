834313ed8006501b733ba240f8496df38cd58a3a
Yohan <yohanpark@medibloc.org>
2018-12-18 21:20:37 +0900

    [Sample] create sample_en module
    - edit a comment in sample_ko˚ˆ

--- sample_en/src/main/java/Main.java
+++ sample_en/src/main/java/Main.java
@@ -19,36 +19,36 @@ public class Main {
     private static final String UPLOAD_DATA = "MyHealthDataForHashingAndUploading";
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Med4j sample 프로그램을 실행합니다.");
+        System.out.println("Start Med4j sample module.");
 
 
-        /*** 1. account 생성, 저장 및 불러오기 ***/
+        /*** 1. create, save, and load account ***/
 
-        // 새로운 account 를 생성합니다.
-        // 옵션이 주어지지 않으면 기본 옵션 값이 설정 됩니다.
+        // Create new account.
+        // Use default option values if an option is not given.
         Account newAccount = AccountUtils.createAccount(PASSWORD, null);
 
-        // 생성한 account 를 주어진 경로에 저장합니다.
-        // 저장되는 파일명은 "UTC--시간--account주소.json" 형식입니다.
+        // Save the account to the given path.
+        // The save file's format is "UTC--time--account_address.json".
         File savedFile = AccountUtils.saveAccount(newAccount, ACCOUNT_FILE_PATH);
         String savedFilePath = savedFile.getAbsolutePath();
 
-        System.out.println("생성한 account 를 저장 하였습니다. 파일명 : " + savedFilePath);
+        System.out.println("The created account is saved. Saved file : " + savedFilePath);
 
-        // 저장된 account 파일로부터 account 정보를 읽습니다.
+        // Load account information from the saved file.
         Account account = AccountUtils.loadAccount(savedFilePath);
 
 
-        /*** 2. data hash 업로드 ***/
+        /*** 2. Upload data hash ***/
 
-        // Blockchain 에 접근하기 위한 med4j client 를 생성 합니다.
-        // med4j client 를 이용하여 Blockchain 과 통신 할 때에는 Rpc(Remote Procedure Call) 패키지 내의 클래스가 사용 됩니다.
+        // Create med4j client to access to MediBloc blockchain.
+        // When communicating with the blockchain, Rpc(Remote Procedure Call) package's classes are used.
         Med4J med4J = Med4J.create(new HttpService(TESTNET_URL));
 
-        // Blockchain 에 업로드 할 data hash 값을 구합니다.
+        // Get hash value of the data for uploading to the blockchain.
         byte[] dataHash = Data.hashRecord(UPLOAD_DATA);
 
-        // Blockchain 에서 account 의 현재 정보를 조회 합니다.
+        // Get account's current state on the blockchain.
         Rpc.GetAccountRequest accountRequest = Rpc.GetAccountRequest.newBuilder()
                 .setAddress(account.getAddress())
                 .setType(ACCOUNT_REQUEST_TYPE_TAIL)
@@ -56,26 +56,26 @@ public class Main {
         Rpc.Account accountBCInfo = med4J.getAccount(accountRequest).send();
         long nextNonce = accountBCInfo.getNonce() + 1;
 
-        // Blockchain 의 chainId 를 조회 합니다. 또는, 환경 설정 파일에 저장한 chainId 를 이용 할 수도 있습니다.
+        // Get the chain ID of the blockchain. You can use a configuration file to save&load the chain ID.
         Rpc.MedState medState = med4J.getMedState().send();
         int chainId = medState.getChainId();
 
-        // Blockchain 에 등록 할 transaction 을 생성 합니다. 생성된 transaction 은 hash 및 sign 의 대상이 됩니다.
+        // Generate a transaction for uploading to the blockchain. The generated transaction will be hashed and signed.
         BlockChain.TransactionHashTarget transactionHashTarget
                 = Transaction.getAddRecordTransactionHashTarget(dataHash, account.getAddress(), nextNonce, chainId);
 
-        // transactionHashTarget 을 hash 하고, 개인키로 sign 합니다. 주어진 account 와 비밀번호는 개인키를 복호화 하는 데 사용 됩니다.
+        // Hash transactionHashTarget and sign it using the private key. The given account and password is used for decrypt the private key.
         Rpc.SendTransactionRequest transactionRequest = Transaction.getSignedTransactionRequest(transactionHashTarget, account, PASSWORD);
 
-        System.out.println("블록체인에 새로운 transaction 을 업로드 합니다.\ntransaction : " + JsonFormat.printer().print(transactionRequest));
+        System.out.println("Upload transaction to blockchain.\ntransaction : " + JsonFormat.printer().print(transactionRequest));
 
-        // Blockchain 에 transaction 을 업로드 하고 결과를 반환 받습니다.
+        // Upload the transaction to blockchain and get the result.
         Rpc.TransactionHash resultHash = med4J.sendTransaction(transactionRequest).send();
 
         if (transactionRequest.getHash().equals(resultHash.getHash())) {
-            System.out.println("요청한 transaction 이 Blockchain transaction pool 에 등록 되었습니다.");
+            System.out.println("The transaction is sent to blockchain transaction pool.");
         } else {
-            throw new Exception("transaction 업로드 중 오류가 발생 하였습니다.");
+            throw new Exception("An error occurred while uploading the transaction.");
         }
 
         return;
